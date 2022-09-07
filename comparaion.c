c086f5a914ee28817d9240c22cad136dc5e7bcdc
unknown <zaaferkenza08@gmail.com>
2022-09-07 16:54:13 +0100

    changes

--- comparaion.c
+++ comparaion.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 void main(){
-    
-    int num1,num2;
-   printf("\n Enter deux valeurs : num1 and num2   :  \n");
-  	scanf("%d%d%d", &num1, &num2);
-    if(num1==num2)
-    {
-        printf("Les deux nombre que vous avez entrée sont égales ");
-
-    }else if (num1>num2){
-        printf(" %d est supeieure a %d",num1,num2);
-
-    }else{
-        printf(" %d est inferieure a %d",num1,num2);
-
-    }
+    char Op;
+	float num1, num2, num3 ;
+	printf("Calcul");
+  	printf("\n Enter un Operator  (= , < , > ) :  ");
+  	scanf("%c", &Op);
+	printf("\n Enter trois valeurs : num1 and num2  and num3 :  \n");
+  	scanf("%f%f%f", &num1, &num2, &num3);
+  	switch(Op)
+  	{
+  		case '=':
+  			if(num1==num2 && num2==num3){
+                printf("Les chiffres que vous avez entrés sont égaux");
+            }else printf("Les chiffres que vous avez entrés ne sont pas égaux");
+  			break;
+  		case '>':
+        if(num1>num2 && num1>num3){
+                printf("Le chifre le plus grand est : %d",num1);
+            }else if(num2>num1 && num2>num3){  printf("Le chifre le plus grand est : %d",num2);}
+            else printf("Le chifre le plus grand est : %d",num3);
+  			break;
+        case '<':
+  			if(num1<num2 && num1<num3){
+                printf("Le chifre le plus grand est : %d",num1);
+            }else if(num2<num1 && num2<num3){  printf("Le chifre le plus grand est : %d",num2);}
+            else printf("Le chifre le plus grand est : %d",num3);
+  			break;
+		default:
+			printf("\n Vous avez entré un opérateur non valide ");				    			
+	}
 }
