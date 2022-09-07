8adbc2a63fdcabd25d0548ae11888435109e1f50
unknown <zaaferkenza08@gmail.com>
2022-09-07 15:50:12 +0100

    hhh

--- comparaion.c
+++ comparaion.c
@@ -3,11 +3,11 @@ void main(){
     int num1,num2;
    printf("\n Enter deux valeurs : num1 and num2   :  \n");
   	scanf("%d%d%d", &num1, &num2);
-    if(((num1==num2)))
+    if(num1==num2)
     {
         printf("Les deux nombre que vous avez entrée sont égales ");
 
-    }else if ((num1>num2)){
+    }else if (num1>num2){
         printf(" %d est supeieure a %d",num1,num2);
 
     }else{
