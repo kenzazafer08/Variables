8850443d3d27ec6bc9833548337281affee63242
unknown <zaaferkenza08@gmail.com>
2022-09-07 15:40:31 +0100

    test

--- max.c
+++ max.c
@@ -3,9 +3,9 @@ void main(){
     int num1,num2,num3;
    printf("\n Enter deux valeurs : num1 and num2 and num3  :  \n");
   	scanf("%d%d%d", &num1, &num2, &num3);
-    if((num1>num2)&&(num1>num3))
+    if(((num1>num2)&&(num1>num3)))
     {
-        printf("Le nombre le plus grand est : %d",num1);
+        printf("Le nombre le plus grand est : %d ",num1);
 
     }else if ((num2>num1)&&(num2>num3)){
         printf("Le nombre le plus grand est : %d",num2);
