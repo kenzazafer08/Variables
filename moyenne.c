3fbeab73f708c18b7c6094d117935b0c8e774024
unknown <zaaferkenza08@gmail.com>
2022-09-07 17:31:51 +0100

    last changes

--- moyenne.c
+++ moyenne.c
@@ -14,19 +14,26 @@ void main()
     {
      printf("Note %d : ", I+1);
      scanf("%d", &A[I]);
+     if( A[I]>20 || A[I]<0){
+   printf("la note que vous avez entrer est non valide \n");
+   continue;
+ }
     }
-    
  /* Affichage du tableau */
  printf("Voici vos notes :\n");
  for (I=0; I<N; I++){
  printf("%d -", A[I]);
+ 
  Somme = Somme+A[I];
- }printf("\n");
+ }
+ printf("\n");
  Moyenne = Somme/N;
  printf("Votre moyenne est: %d ,",Moyenne);
  if(Moyenne>=12){
     printf("vous etes admis");
- }else if(Moyenne<12 && Moyenne >8){
+ }
+ else if(Moyenne<12 && Moyenne >8){
     printf("vous etes redoublant");
- }else printf("vous etes exclu");
+ }
+ else printf("vous etes exclu");
 }
