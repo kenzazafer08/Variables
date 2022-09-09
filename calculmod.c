bf25a617a3f5654e4aeb2f86421f02169eea9ba9
unknown <zaaferkenza08@gmail.com>
2022-09-09 17:57:36 +0100

    done

--- calculmod.c
+++ calculmod.c
@@ -48,21 +48,27 @@ void mathematique(){
             printf("\n Le resultat de %.2f / %.2f  = %.2f", num1, num2, result);
             } else printf("Erreur !");
             break;
-        case 13:
-        int mod,n,m; 
-        printf("\n Enter les deux valeurs que vous voulez calculer :  \n");
-  	    scanf("%d%d", &n, &m);
-        if(num2!=0){
-            mod = n % m;
-            printf("\n Le rest de %d / %d  = %d", n, m, mod);
-            } else printf("Erreur !");
-            break;  
         case 5:
             printf("\n Enter les deux valeurs que vous voulez calculer :  \n");
   	        scanf("%f%f", &num1, &num2); 
   			result = pow(num1,num2);
             printf("\n La puissance de %.2f a %.2f  = %.2f", num1, num2, result);
   			break;
+        case 6:
+            printf("\n Enter une valeur que vous voulez calculer :  \n");
+  	        scanf("%f", &num1); 
+  			result = sqrt(num1);
+            printf("\n Le racine caree de %.2f  = %.2f", num1, result);
+  			break;
+        case 7:
+            printf("\n Enter une valeur que vous voulez calculer :  \n");
+  	        scanf("%f", &num1);
+  			int i,r=1;
+            for(i=2;i<=num1;i++){
+                r=r*i;
+            }result = r;
+            printf("\n Le factorielle de  %.2f = %.2f\n", num1, result);
+  			break; 
         case 8:
             printf("\n Enter une valeur que vous voulez calculer :  \n");
   	        scanf("%f", &num1); 
@@ -81,33 +87,28 @@ void mathematique(){
   			result = sin(num1);
             printf("\n Le sin de %.2f  = %.2f", num1, result);
   			break;
-        case 12:
-            printf("\n Enter une valeur que vous voulez calculer :  \n");
-  	        scanf("%f", &num1); 
-  			result = cos(num1);
-            printf("\n Le cos de %.2f  = %.2f", num1, result);
-  			break;
         case 11:
             printf("\n Enter une valeur que vous voulez calculer :  \n");
   	        scanf("%f", &num1); 
   			result = tan(num1);
             printf("\n Le tan de %.2f  = %.2f", num1, result);
   			break;
-        case 6:
+        case 12:
             printf("\n Enter une valeur que vous voulez calculer :  \n");
   	        scanf("%f", &num1); 
-  			result = sqrt(num1);
-            printf("\n Le racine caree de %.2f  = %.2f", num1, result);
+  			result = cos(num1);
+            printf("\n Le cos de %.2f  = %.2f", num1, result);
   			break;
-        case 7:
-            printf("\n Enter une valeur que vous voulez calculer :  \n");
-  	        scanf("%f", &num1);
-  			int i,r=1;
-            for(i=2;i<=num1;i++){
-                r=r*i;
-            }result = r;
-            printf("\n Le factorielle de  %.2f = %.2f\n", num1, result);
-  			break; 
+        case 13:
+        int mod,n,m; 
+        printf("\n Enter les deux valeurs que vous voulez calculer :  \n");
+  	    scanf("%d%d", &n, &m);
+        if(num2!=0){
+            mod = n % m;
+            printf("\n Le rest de %d / %d  = %d", n, m, mod);
+            } else printf("Erreur !");
+            break; 
+        
 		default:
 			printf("\n Vous avez entrer un operateur non valide ");				    			
 	}
